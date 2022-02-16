42825282429bbe2c40fb091dae353723b276896e
linkin2b <b0skiewheels@gmail.com>
2022-02-16 15:35:04 -0800

    api usage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = "https://steam-store-data.p.rapidapi.com/api/featured/";
+
+class Top extends React.Component {
+
+constructor(props){
+  super(props);
+
+  this.state = {
+    games: [],
+    name: '',
+    small_capsule_image: ''
+  };
 }
 
-export default App;
+componentDidMount() {
+  this.getData()
+}
+
+async getData(){
+const options = {
+    
+  method: 'GET',
+  url: 'https://steam-store-data.p.rapidapi.com/api/featured/',
+  headers: {
+    'x-rapidapi-host': 'steam-store-data.p.rapidapi.com',
+    'x-rapidapi-key': '44724f4afcmshd696a5173f1e582p103d8ejsn05f14162c664'
+  }
+};
+
+const res = await axios.request(options)
+const {data} = await res;
+  
+this.setState({games: data.featured_win})
+}
+
+
+
+
+// create card for each enttry
+
+//lines 64-71
+
+
+render (){
+  const {games} = this.state;
+ console.log(games)
+
+  return(
+  
+
+  <div>
+    <div>
+    <h1 id="heading">
+      Steam Featured Windows Games
+    </h1></div>
+      
+        {
+          games.length && games.map( games =>  {
+            return (<div id="container">
+                      <div id="card">
+                      < h2>{games.name}</h2>
+                        <div>
+                          <img id="img" src={games.small_capsule_image} alt="Number 1" ></img>
+                        </div>
+                        <div><h3>${games.final_price}</h3></div>
+                      </div>
+                      </div>)
+                  
+          })
+        }        
+        
+  </div>
+  )
+}
+}
+
+export default Top;
